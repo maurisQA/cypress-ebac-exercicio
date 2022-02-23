32b8991a0a98501384f5f949e750a6747fd1a62f
Mauricio Figueiredo <mauricio.figueiredo@marttech.com.br>
2022-02-23 13:03:50 -0300

    usando massa de dados

---
+++ cypress/integration/endereco.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
+beforeEach(() => {
+    cy.visit('minha-conta')
+});
+
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
+        //login 
+        //cadastro de endereço
+    });
+});
