d7a712f575087890f0a6eb59ece8642f897f3c20
Mauricio Figueiredo <mauricio.figueiredo@marttech.com.br>
2022-03-14 15:10:35 -0300

    lista de endereços

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,9 +1,11 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereço.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
         cy.visit('minha-conta')
-        cy.fixture('perfil').then(dados =>{
+        cy.fixture('perfil').then(dados => {
             cy.login(dados.usuario, dados.senha)
         })
 
@@ -11,7 +13,27 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.EditarEnderecoFaturamento('Mauricio', 'Grown', 'Marttech', 'Brasil', 'Tenente Cassimiro Dias', '666', 'Martinópolis', 'São Paulo', '19500000', '18997523389', 'eu@eu.com.ru')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
 
-        //cadastro de endereço
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.EditarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            
+            
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+
     });
 });
